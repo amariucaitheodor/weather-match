fc6be9c39340976789af20a6b893fc279930fc0d
Theodor Amariucai <32778667+amariucaitheodor@users.noreply.github.com>
2021-12-10 10:12:53 +0100

    Update node.js.yml

---
+++ client/src/assets/index.js
@@ -0,0 +1,64 @@
+import atmosphere1 from './Atmosphere/1.webp';
+import clear_day0 from './Clear/Day/0.webp';
+import clear_day1 from './Clear/Day/1.webp';
+import clear_day2 from './Clear/Day/2.webp';
+import clear_day3 from './Clear/Day/3.webp';
+import clear_day4 from './Clear/Day/4.webp';
+import clear_day5 from './Clear/Day/5.webp';
+import clear_day6 from './Clear/Day/6.webp';
+import clear_day7 from './Clear/Day/7.webp';
+import clear_day8 from './Clear/Day/8.webp';
+import clear_day9 from './Clear/Day/9.webp';
+import clear_night1 from './Clear/Day/1.webp';
+import clear_night2 from './Clear/Day/2.webp';
+import clear_night3 from './Clear/Day/3.webp';
+import clear_night4 from './Clear/Day/4.webp';
+import clear_night5 from './Clear/Day/5.webp';
+import clear_night6 from './Clear/Day/6.webp';
+import clouds_day1 from './Clouds/Day/1.webp';
+import clouds_day2 from './Clouds/Day/2.webp';
+import clouds_day3 from './Clouds/Day/3.webp';
+import clouds_day4 from './Clouds/Day/4.webp';
+import clouds_day5 from './Clouds/Day/5.webp';
+import clouds_day6 from './Clouds/Day/6.webp';
+import clouds_day7 from './Clouds/Day/7.webp';
+import clouds_night1 from './Clouds/Night/1.webp';
+import clouds_night2 from './Clouds/Night/2.webp';
+import clouds_night3 from './Clouds/Night/3.webp';
+import clouds_night4 from './Clouds/Night/4.webp';
+import clouds_night5 from './Clouds/Night/5.webp';
+import clouds_night6 from './Clouds/Night/6.webp';
+import clouds_night7 from './Clouds/Night/7.webp';
+import clouds_night8 from './Clouds/Night/8.webp';
+import clouds_night9 from './Clouds/Night/9.webp';
+import rain1 from './Rain/1.webp';
+import rain2 from './Rain/2.webp';
+import rain3 from './Rain/3.webp';
+import rain4 from './Rain/4.webp';
+import rain5 from './Rain/5.webp';
+import rain6 from './Rain/6.webp';
+import snow1 from './Snow/1.webp';
+import snow2 from './Snow/2.webp';
+import snow3 from './Snow/3.webp';
+import snow4 from './Snow/4.webp';
+import thunderstorm1 from './Thunderstorm/1.webp';
+
+var atmosphere = [atmosphere1];
+var clear_day = [clear_day0, clear_day1, clear_day2, clear_day3, clear_day4, clear_day5, clear_day6, clear_day7, clear_day8, clear_day9];
+var clear_night = [clear_night1, clear_night2, clear_night3, clear_night4, clear_night5, clear_night6];
+var clouds_day = [clouds_day1, clouds_day2, clouds_day3, clouds_day4, clouds_day5, clouds_day6, clouds_day7];
+var clouds_night = [clouds_night1, clouds_night2, clouds_night3, clouds_night4, clouds_night5, clouds_night6, clouds_night7, clouds_night8, clouds_night9];
+var rain = [rain1, rain2, rain3, rain4, rain5, rain6];
+var snow = [snow1, snow2, snow3, snow4];
+var thunderstorm = [thunderstorm1];
+
+export {
+    atmosphere,
+    clear_day,
+    clear_night,
+    clouds_day,
+    clouds_night,
+    rain,
+    snow,
+    thunderstorm
+  };
